205bf373eba4c570322f320756655de143bd1778
Romain Tello <romain.tello@gmail.com>
2019-12-20 16:36:47 +0100

    Add find method to User class

--- src/app/App.java
+++ src/app/App.java
@@ -4,8 +4,12 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Order myOrder = Order.find(2);
-        myOrder.inspect();
-        myOrder.getUser().inspect();
+        
+        User myUser = User.find(4);
+        myUser.inspect();
+
+        //Order myOrder = Order.find(2);
+        //myOrder.inspect();
+        //myOrder.getUser().inspect();
     }
 }
--- src/app/User.java
+++ src/app/User.java
@@ -1,10 +1,32 @@
 package app;
 
+import java.sql.*;
+
 class User {
     public int id;
     public String email;
     public String password;
     public String username;
+
+    static public User find(int id) throws SQLException {
+        ResultSet resultSet = DBManager.sendQuery(
+            "SELECT * FROM `user` WHERE `id` = " + id
+        );
+
+        resultSet.last();
+        if (resultSet.getRow() == 0) {
+            throw new RuntimeException("Id " + id + " doesn't exist in table `user`");
+        }
+
+        resultSet.first();
+
+        return new User(
+            resultSet.getInt(1),
+            resultSet.getString(2),
+            resultSet.getString(3),
+            resultSet.getString(4)
+        );
+    }
 
     User(int id, String email, String password, String username) {
         this.id = id;
