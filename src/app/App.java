47a39b6ee380088203aabf0e337c53cc36cc07b2
Romain Tello <romain.tello@gmail.com>
2019-12-20 14:05:15 +0100

    Write SQL query #3

--- src/app/App.java
+++ src/app/App.java
@@ -4,12 +4,15 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ResultSet products = DBManager.sendQuery(
-            "SELECT COUNT(`id`) FROM `product` WHERE `brand_id` = 1"
+        ResultSet orders = DBManager.sendQuery(
+            "SELECT * FROM `_order` WHERE `user_id` = 5"
         );
 
-        while (products.next()) {
-            System.out.println(products.getInt(1));
+        while (orders.next()) {
+            System.out.println(
+                "Order #" + orders.getString(1) +
+                ": " + orders.getString(2)
+            );
         }
     }
 }
