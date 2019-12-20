bac92c1da6f8912e523060b7753b7d19004e0ff4
Romain Tello <romain.tello@gmail.com>
2019-12-20 15:56:27 +0100

    Add find method to Order class

--- src/app/App.java
+++ src/app/App.java
@@ -4,18 +4,6 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ResultSet products = DBManager.sendQuery(
-            "SELECT * FROM `product` " +
-            "JOIN `orders_in_products` ON `product`.`id` = `orders_in_products`.`product_id`" +
-            "JOIN `_order` ON `_order`.`id` = `orders_in_products`.`order_id` " +
-            "WHERE `_order`.`user_id` = 2"
-        );
-
-        while (products.next()) {
-            System.out.println(
-                "Product #" + products.getString(1) +
-                ": " + products.getString(3)
-            );
-        }
+        Order myOrder = Order.find(1);
     }
 }
--- src/app/Order.java
+++ src/app/Order.java
@@ -1,12 +1,27 @@
 package app;
 
-import java.sql.Date;
+import java.sql.*;
 
 class Order {
     public int id;
     public String reference;
     public Date date;
     public int status;
+
+    static public Order find(int id) throws SQLException {
+        ResultSet resultSet = DBManager.sendQuery(
+            "SELECT * FROM `_order` WHERE `id` = " + id
+        );
+
+        resultSet.next();
+
+        return new Order(
+            resultSet.getInt(1),
+            resultSet.getString(2),
+            resultSet.getDate(3),
+            resultSet.getInt(4)
+        );
+    }
 
     Order(int id, String reference, Date date, int status) {
         this.id = id;
