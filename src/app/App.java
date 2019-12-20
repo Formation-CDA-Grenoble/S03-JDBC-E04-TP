9b532d05e0f525767b266ed35bf4286398cd9c48
Romain Tello <romain.tello@gmail.com>
2019-12-20 15:09:11 +0100

    Write SQL query #7

--- src/app/App.java
+++ src/app/App.java
@@ -4,14 +4,18 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ResultSet productsInOrder = DBManager.sendQuery(
-            "SELECT COUNT(`orders_in_products`.`id`) FROM `orders_in_products` " +
-            "JOIN `_order` ON `orders_in_products`.`order_id` = `_order`.`id` " +
-            "WHERE `_order`.`user_id` = 1"
+        ResultSet products = DBManager.sendQuery(
+            "SELECT * FROM `product` " +
+            "JOIN `orders_in_products` ON `product`.`id` = `orders_in_products`.`product_id`" +
+            "JOIN `_order` ON `_order`.`id` = `orders_in_products`.`order_id` " +
+            "WHERE `_order`.`user_id` = 2"
         );
 
-        while (productsInOrder.next()) {
-            System.out.println(productsInOrder.getInt(1));
+        while (products.next()) {
+            System.out.println(
+                "Product #" + products.getString(1) +
+                ": " + products.getString(3)
+            );
         }
     }
 }
