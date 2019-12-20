1f4b958cc1c6f162543de6abe37ff328312e6545
Romain Tello <romain.tello@gmail.com>
2019-12-20 14:53:47 +0100

    Change SQL query #6 to count

--- src/app/App.java
+++ src/app/App.java
@@ -5,16 +5,13 @@ import java.sql.*;
 public class App {
     public static void main(String[] args) throws Exception {
         ResultSet productsInOrder = DBManager.sendQuery(
-            "SELECT * FROM `orders_in_products` " +
+            "SELECT COUNT(`orders_in_products`.`id`) FROM `orders_in_products` " +
             "JOIN `_order` ON `orders_in_products`.`order_id` = `_order`.`id` " +
             "WHERE `_order`.`user_id` = 1"
         );
 
         while (productsInOrder.next()) {
-            System.out.println(
-                "Product in order #" + productsInOrder.getString(1) +
-                ": " + productsInOrder.getString(3)
-            );
+            System.out.println(productsInOrder.getInt(1));
         }
     }
 }
